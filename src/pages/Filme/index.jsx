a84ba9357a96a16323d93b2246070af1b6fb164d
phosantos <pholiveirasantos1091@gmail.com>
2022-10-20 00:37:13 -0300

    atualizacao pagina filme

--- src/pages/Filme/index.jsx
+++ src/pages/Filme/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styles from './Filme.module.css';
-import { useParams } from 'react-router-dom';
+import { useParams, redirect } from 'react-router-dom';
+import { ReactComponent as Star } from '../../assets/StarOutline.svg';
+import Slider from '../../components/Slider';
 
 const Filme = () => {
   const { id } = useParams();
   const [data, setData] = React.useState();
+  const [recommendations, setRecommendations] = React.useState();
 
   React.useEffect(() => {
     async function fetchData() {
@@ -16,62 +19,75 @@ const Filme = () => {
 
       const json = await response.json();
 
-      console.log(json);
+      const responseR = await fetch(
+        `https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=${
+          import.meta.env.VITE_API_KEY
+        }&language=pt-BR`,
+      );
+
+      const recomendations = await responseR.json();
+
+      setRecommendations(recomendations.results);
       setData(json);
     }
 
     fetchData();
-  }, []);
+  }, [id]);
 
-  // React.useEffect(() => {
-  //   const bg = document.querySelector('.bg');
-  //   if (bg && data)
-  //     bg.style.backgroundImage = `url("https://image.tmdb.org/t/p/w1280${data.backdrop_path}")`;
-  // }, [data]);
-
-  React.useLayoutEffect(() => {
+  React.useEffect(() => {
     const bg = document.querySelector('.bg');
     if (bg && data)
       bg.style.backgroundImage = `url("https://image.tmdb.org/t/p/w1280${data.backdrop_path}")`;
   }, [data]);
 
   //se n existir redirecionar p 404
-  return (
-    <main>
-      <section className={styles.movieInfo}>
-        <div className={styles.posterWrapper}></div>
-        <div className={styles.info}>
-          <div className={styles.titleWrapper}>
-            <h1 className={styles.title}>As Aventuras de Pi</h1>
-            <span className={styles.year}>(2012)</span>
-          </div>
-          <p className={styles.genres}>
-            Animação, Comédia, Sci-Fi & Fantasy, Action & Adventure • 22m
-          </p>
-          <div className={styles.rating}>Estrela 4.5</div>
-          <div className={styles.synopsis}>
-            <h3>Sinopse</h3>
-            <p>
-              Rick é um velho mentalmente desequilibrado, mas cientificamente
-              talentoso que se reconectou recentemente com sua família. Ele
-              passa a maior parte do tempo envolvendo o jovem neto Morty em
-              aventuras perigosas e estranhas no espaço e em universos
-              alternativos. Compostos à vida familiar já instável de Morty,
-              esses eventos causam muito sofrimento a Morty em casa e na escola.
-            </p>
-          </div>
-        </div>
-      </section>
+  //
+  //
 
-      <section>
-        <h2>Elenco</h2>
-      </section>
+  if (data)
+    return (
+      <main>
+        <section className={styles.movieInfo}>
+          <div
+            className={styles.posterWrapper}
+            style={{
+              backgroundImage: `url("https://image.tmdb.org/t/p/w300${data.poster_path}")`,
+            }}
+          ></div>
+          <div className={styles.info}>
+            <h1 className={styles.title}>{data.title}</h1>
+            <div className={styles.genresWrapper}>
+              <span className={styles.rating}>
+                <Star /> {data.vote_average.toFixed(1)}
+              </span>
+              <span>•</span>
+              <span>{data.release_date.slice(0, 4)}</span>
+              <span>•</span>
+              <ul className={styles.genres}>
+                {data.genres.map((genre, index, array) => {
+                  if (index === array.length - 2) {
+                    return <li key={genre.id}>{genre.name} & </li>;
+                  }
+                  if (index === array.length - 1) {
+                    return <li key={genre.id}>{genre.name}</li>;
+                  }
+                  return <li key={genre.id}>{genre.name}, </li>;
+                })}
+              </ul>
+            </div>
+            <p className={styles.overview}>{data.overview}</p>
+          </div>
+        </section>
 
-      <section className={styles.recommendations}>
-        <h2>Recomendações</h2>
-      </section>
-    </main>
-  );
+        <section className={styles.recommendations}>
+          <h2>Recomendações</h2>
+          {console.log(recommendations)}
+          <Slider movies={recommendations} />
+        </section>
+      </main>
+    );
+
+  return null;
 };
 
 export default Filme;
