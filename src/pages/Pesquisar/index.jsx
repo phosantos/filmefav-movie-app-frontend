1628fffc8d5130255e0c7d83703050368d9f3051
phosantos <pholiveirasantos1091@gmail.com>
2022-10-23 03:41:27 -0300

    searchParams pesquisa

--- src/pages/Pesquisar/index.jsx
+++ src/pages/Pesquisar/index.jsx
@@ -2,42 +2,60 @@ import React from 'react';
 import styles from './Pesquisar.module.css';
 import { ReactComponent as Search } from '../../assets/search.svg';
 import MovieItem from '../../components/MovieItem';
+import { useSearchParams } from 'react-router-dom';
 
 const Pesquisar = () => {
   const [search, setSearch] = React.useState('');
+
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const [data, setData] = React.useState(null);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
 
-  async function handleSearch() {
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${
-      import.meta.env.VITE_API_KEY
-    }&language=pt-BR&query=${search}&page=1&include_adult=false`;
-    try {
-      setLoading(true);
-      const res = await fetch(url);
-      const json = await res.json();
-      setData(json);
-      console.log(json);
-      setLoading(false);
-    } catch (error) {
-      setLoading(false);
-      setError(error);
-      setData(null);
+  React.useEffect(() => {
+    const query = searchParams.get('s');
+    async function fetcher() {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${
+        import.meta.env.VITE_API_KEY
+      }&language=pt-BR&query=${query}&page=1&include_adult=false`;
+      try {
+        setLoading(true);
+        const res = await fetch(url);
+        const json = await res.json();
+        setData(json);
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        setError(error);
+        setData(null);
+      }
     }
+    if (query !== null) {
+      fetcher();
+    }
+  }, [searchParams]);
+
+  async function handleSearchParams(e) {
+    e.preventDefault();
+    const query = { s: search };
+    setSearchParams(query);
+
+    // const url = new URL(window.location);
+    // url.searchParams.set('search', search);
+    // window.history.pushState({}, '', url);
   }
 
   return (
     <main>
-      <form className={styles.search} onSubmit={(e) => e.preventDefault()}>
+      <form className={styles.search} onSubmit={handleSearchParams}>
         <input
           type="text"
           placeholder="Nome do filme..."
           value={search}
           onChange={({ target }) => setSearch(target.value)}
         />
-        <button onClick={handleSearch}>
+        <button>
           <Search />
         </button>
       </form>
